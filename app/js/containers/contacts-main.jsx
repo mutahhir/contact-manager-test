acb6e39eacae6ac06e93c0e62ccc0177474bef86
Mutahhir <mutahhir.ali@gmail.com>
2015-10-20 23:36:02 +0500

    Move initial state to contacts reducer

--- app/js/containers/contacts-main.jsx
+++ app/js/containers/contacts-main.jsx
@@ -1,58 +1,28 @@
 import React from 'react';
-import ContactsCollection from '../collections/contacts';
+import { connect } from 'react-redux';
 
 import Contacts from '../components/contacts.jsx';
 
-let contacts = [
-  {
-    id: 1,
-    name: 'Terrence S. Hatfield',
-    tel: '651-603-1723',
-    email: 'TerrenceSHatfield@rhyta.com'
-  },
-  {
-    id: 2,
-    name: 'Chris M. Manning',
-    tel: '513-307-5859',
-    email: 'ChrisMManning@dayrep.com'
-  },
-  {
-    id: 3,
-    name: 'Ricky M. Digiacomo',
-    tel: '918-774-0199',
-    email: 'RickyMDigiacomo@teleworm.us'
-  },
-  {
-    id: 4,
-    name: 'Michael K. Bayne',
-    tel: '702-989-5145',
-    email: 'MichaelKBayne@rhyta.com'
-  },
-  {
-    id: 5,
-    name: 'John I. Wilson',
-    tel: '318-292-6700',
-    email: 'JohnIWilson@dayrep.com'
-  },
-  {
-    id: 6,
-    name: 'Rodolfo P. Robinett',
-    tel: '803-557-9815',
-    email: 'RodolfoPRobinett@jourrapide.com'
-  }
-];
-
 const ContactsMain = React.createClass({
 
-  getInitialState () {
-    return {
-      contacts: new ContactsCollection(contacts)
-    };
+  propTypes: {
+    contacts: React.PropTypes.object.isRequired
   },
 
   render () {
-    return <Contacts contacts={this.state.contacts}></Contacts>;
+    const {contacts} = this.props;
+    return <Contacts contacts={contacts}></Contacts>;
   }
 });
 
-export default ContactsMain;
+function mapStateToProps(state) {
+  return {
+    contacts: state.contacts
+  };
+}
+
+
+
+export default connect(
+  mapStateToProps
+)(ContactsMain);
