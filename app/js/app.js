ec8acb7f07737264baa17da30167359fe53a354e
Mutahhir <mutahhir.ali@gmail.com>
2015-10-21 01:14:24 +0500

    Clean up app.js

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,9 @@
-import React from 'react';
+import React from 'react';  // although it isn't being used, we need to import it first
 import $ from 'jquery';
 import { render } from 'react-dom';
-import { Router, Route, Link, Redirect } from 'react-router';
 import { Provider } from 'react-redux';
 import { ReduxRouter } from 'redux-router';
 import configureStore from './store/configure-store';
-
-import ContactsMain from './containers/contacts-main.jsx';
-import ContactsNewEdit from './containers/contacts-new-edit.jsx';
 
 const store = configureStore();
 
@@ -15,66 +11,4 @@ render((
   <Provider store={store}>
     <ReduxRouter />
   </Provider>
-), $(".app")[0]);
-
-/*
-let ContactManager = {
-  Models: {},
-  Collections: {},
-  Views: {},
-
-  start: function() {
-    var router = new Router();
-
-    router.on('route:home', function() {
-      router.navigate('contacts', {
-        trigger: true,
-        replace: true
-      });
-    });
-
-    router.on('route:showContacts', function() {
-      var contactsView = new ContactsView({
-        collection: contacts
-      });
-
-      $('.main-container').html(contactsView.render().$el);
-    });
-
-    router.on('route:newContact', function() {
-      var newContactForm = new ContactForm({
-        model: new ContactModel()
-      });
-
-      newContactForm.on('form:submitted', function(attrs) {
-        attrs.id = contacts.isEmpty() ? 1 : (_.max(contacts.pluck('id')) + 1);
-        contacts.add(attrs);
-        router.navigate('contacts', true);
-      });
-
-      $('.main-container').html(newContactForm.render().$el);
-    });
-
-    router.on('route:editContact', function(id) {
-      var contact = contacts.get(id),
-        editContactForm;
-
-      if (contact) {
-        editContactForm = new ContactManager.Views.ContactForm({
-          model: contact
-        });
-
-        editContactForm.on('form:submitted', function(attrs) {
-          contact.set(attrs);
-          router.navigate('contacts', true);
-        });
-
-        $('.main-container').html(editContactForm.render().$el);
-      } else {
-        router.navigate('contacts', true);
-      }
-    });
-
-    Backbone.history.start();
-  }
-};*/
+), $('.app')[0]);
