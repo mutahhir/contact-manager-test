29794404dbcfe2a823fff1e70dc8fcf67c9726a9
Mutahhir <mutahhir.ali@gmail.com>
2015-10-21 08:18:02 +0500

    WIP: Convert contacts to cards and move actions

--- app/js/components/contact.jsx
+++ app/js/components/contact.jsx
@@ -18,32 +18,35 @@ export default React.createClass({
   render: function () {
     const {id, name, avatar, tel, email} = this.props.contact;
     return (
-      <li className="media col-md-6 col-lg-4">
-        <div className="thumbnail">
-          <img className="media-object" src={`img/faces/${avatar}`} />
-        </div>
-        <div className="media-heading">
-          <h3>
-            {name}
-            <small>
+      <li className="col-md-6 col-lg-4">
+        <div className="card">
+          <div className="card-header">
+            <div className="thumbnail">
+              <img className="media-object" src={`img/faces/${avatar}`} />
+            </div>
+            <div className="actions">
               <Link to={`/contacts/edit/${id}`}>
                 <span className="glyphicon glyphicon-pencil"></span>
               </Link>
               <a className="delete-contract" onClick={this.onDelete}>
                 <span className="glyphicon glyphicon-trash"></span>
               </a>
-            </small>
-          </h3>
-        </div>
-        <div className="media-body">
-          <dl>
-            <dt>Phone Number:</dt>
-            <dd>{tel}</dd>
-            <dt>Email:</dt>
-            <dd>{email}</dd>
-          </dl>
-        </div>
-        <hr />
+            </div>
+          </div>
+          <div className="card-details">
+            <div className="media-heading">
+              <h3>{name}</h3>
+            </div>
+            <div className="media-body">
+              <dl>
+                <dt>Phone Number:</dt>
+                <dd>{tel}</dd>
+                <dt>Email:</dt>
+                <dd>{email}</dd>
+              </dl>
+            </div>
+          </div>
+         </div>
       </li>
     );
   }
